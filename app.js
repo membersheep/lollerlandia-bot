60460938f40c61f3cdb52fb984fc5aae1592153f
Alessandro Maroso <alessandro.maroso@iks.it>
2015-12-28 16:29:05 +0100

    Bugfix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 
 var statusHandler = require('./routes/status');
 
-var telegramRoute = require('./routes/telegram');
+var telegramHandler = require('./routes/telegram');
 
 var app = express();
 
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/status', statusHandler);
-app.post('/telegramBot', telegramRoute);
+app.post('/telegramBot', telegramHandler);
 
 var server = app.listen(SERVER_PORT, function () {
   var host = server.address().address;
--- modules/Bot.js
+++ modules/Bot.js
@@ -1,8 +1,14 @@
 var chanService = require('./4ChanService');
 var telegramService = require('./TelegramAPI');
 
-function bot() {
-  return  {
+telegramService.setupWebhook('158621575:AAEUlrWtGVzdNlAO7FT238J507ogOZJvfKc', 'https://aqueous-lowlands-1093.herokuapp.com/telegramBot', function(err) {
+  if (err) {
+    return console.log(err);
+  }
+  return console.log('Bot successfully set up.');
+});
+
+var bot = {
     readMessage: function(message) {
       if (message.text == '/b') {
         chanService.getRandomImage(message.text, function(err, localPath){
@@ -11,6 +17,8 @@ function bot() {
       }
     }
   };
-}
 
-module.exports = bot();
+
+
+
+module.exports = bot;
