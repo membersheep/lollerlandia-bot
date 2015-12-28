36a8b8e1b218d1549bc764b7e530aa22ad7feaf1
Alessandro Maroso <alessandro.maroso@iks.it>
2015-12-28 16:36:13 +0100

    --

--- routes/telegram.js
+++ routes/telegram.js
@@ -1,7 +1,7 @@
 var bot = require('../modules/Bot.js');
 
 module.exports = function(req, res) {
-  console.console.log(req.body);
+  console.log(req.body);
   var result = req.body;
   for (var update in result) {
     if (result.hasOwnProperty(update)) {
