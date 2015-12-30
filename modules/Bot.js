abe3f7324aa7b44f33eeb55b374aa0df8c4dc7a1
Alessandro Maroso <alessandro.maroso@iks.it>
2015-12-30 17:48:34 +0100

    Try to fix date difference.

--- modules/Bot.js
+++ modules/Bot.js
@@ -76,8 +76,9 @@ bot.isMessageNew = function(message) {
   var currentDate = Date.now();
   var ONE_MINUTE = 60 * 60 * 1000;
   console.log(currentDate/1000);
-  console.log(message.date);
-  return (currentDate - message.date) < ONE_MINUTE;
+  console.log(currentDate/1000 - message.date);
+  console.log(ONE_MINUTE);
+  return (currentDate/1000 - message.date) < ONE_MINUTE;
 };
-console.log(Date.now());
+
 module.exports = bot;
